cfe2dc0a93904091e7c14ccd63d85bda6794708c
zakaria el kachradi <goyor@zakarias-MacBook-Pro.local>
2025-01-03 14:40:36 +0100

    fix img

--- components/logo/Logo.tsx
+++ components/logo/Logo.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Logo = () => {
   return (
     <Link href={'/'} className='flex items-center gap-2 mr-3 md:mr-0'>
-        <Image src={'/logo3.PNG'} width={100} height={80} alt='brand'/>
+        <Image src={'/logo3.png'} width={100} height={80} alt='brand'/>
         
     </Link>
   )
